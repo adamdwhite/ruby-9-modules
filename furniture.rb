7aa0957ba47313573304244fa1747e29e2efdf74
Adam D. White <adamwhite82@gmail.com>
2017-10-10 10:04:48 -0500

    ditto

--- furniture.rb
+++ furniture.rb
@@ -1,7 +1,10 @@
 class Furniture
     
-      def total_size(length, width, height)
+    def total_size(length, width, height)
         puts "This piece has the following dimensions: #{width}W x #{height}H x #{length}L"
-      end
-    
+        @width = width
+        @height = height 
+        @length = length
+    end
+
     end
--- table.rb
+++ table.rb
@@ -1,1 +1,26 @@
-# 3. Create a class Table that inherits from class Furniture and also has added variable for seating(Integer). Use the module Properties as a mixin in the class Table in order to determine its number of legs, type of wood, dimensions and description.
+# Create a class Table that inherits from class Furniture and also has added variable for seating(Integer). Use the module Properties as a mixin in the class Table in order to determine its number of legs, type of wood, dimensions and description.
+require_relative 'properties'
+require_relative 'table'
+require_relative 'furniture'
+
+
+class Table < Furniture
+    include Properties
+    # puts "hello from the Properties within table.rb"
+
+    def intialize(seating)
+        @seating = seating
+    end 
+
+    def seating 
+        seats = 7
+        puts "This table has seating for #{seats}"
+    end 
+    
+end
+
+table = Table.new
+
+puts table.seating 
+puts table.description
+puts table.total_size
